544ee0a4ce0c96aa79c1404fb1c1c2fac7bacb0e
Maksym Piavka <maxpyavka@gmail.com>
2022-03-29 21:32:19 +0200

    grpc tests

--- src/bin/rpc_server.rs
+++ src/bin/rpc_server.rs
@@ -1,37 +1,13 @@
 use tonic::transport::Server;
-use tonic::{Request, Response, Status};
 use log::info;
 
-use discord_pipe::prototypes::{
-    DiscordPipe, DiscordPipeServer, DiscordPushResult, MessageToChannel,
-};
+use discord_pipe::prototypes::DiscordPipeServer;
 
 use env_logger;
 
 use discord_pipe::discord_push_handler::DiscordBot;
 use discord_pipe::utils::{get_discord_bot_token, get_service_socket};
-
-#[derive(Debug)]
-pub struct DiscordPipeService {
-    discord_bot: DiscordBot,
-}
-
-#[tonic::async_trait]
-impl DiscordPipe for DiscordPipeService {
-    async fn push_message(
-        &self,
-        request: Request<MessageToChannel>,
-    ) -> Result<Response<DiscordPushResult>, Status> {
-        let error_message = match self.discord_bot.push_to_channel(request.into_inner()).await {
-            Err(e) => { Some(e.to_string()) },
-            _ => {None}
-        };
-
-        Ok(Response::new(DiscordPushResult {
-            error_message
-        }))
-    }
-}
+use discord_pipe::service::DiscordPipeService;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
