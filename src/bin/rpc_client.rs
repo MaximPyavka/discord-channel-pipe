6d24f9a723db445a55099345444c74576fbd54a1
Maksym Piavka <maxpyavka@gmail.com>
2022-04-03 15:34:30 +0200

    Renamed rpc-client

---
+++ src/bin/rpc_client.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use tonic::transport::Channel;
+use tonic::Request;
+
+use discord_pipe::prototypes::{MessageToChannel, DiscordPipeClient};
+use discord_pipe::utils::{get_client_endpoint, get_channel_id};
+
+
+async fn test_call(client: &mut DiscordPipeClient<Channel>) -> Result<(), Box<dyn Error>> {
+    let message = MessageToChannel {channel_id: get_channel_id(), content: "Some message!".to_string()};
+    
+    let response = client
+        .push_message(Request::new(message))
+        .await;
+
+    println!("Response: {:?}", response);
+
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let service_addr = get_client_endpoint();
+    let mut client = DiscordPipeClient::connect(service_addr).await?;
+    test_call(&mut client).await?;
+    Ok(())
+}
